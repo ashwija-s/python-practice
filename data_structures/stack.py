79a2f26a0928ef3745c7c3efde244ca21b24ad9e
ashwija-s <ashwijashetty06@gmail.com>
2026-07-02 12:09:39 +0530

    Added stack practice example

---
+++ data_structures/stack.py
@@ -0,0 +1,13 @@
+stack = []
+
+stack.append("Apple")
+stack.append("Banana")
+stack.append("Cherry")
+
+print("Before: ", stack)
+
+remove = stack.pop()
+
+print("Removed ", remove)
+
+print("After: ", stack)
