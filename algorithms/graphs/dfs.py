b0edc74bc1a5ecbbd0a2897a0dc519180abbf23f
ashwija-s <ashwijashetty06@gmail.com>
2026-07-08 17:27:15 +0530

    Add DFS and BFS

---
+++ algorithms/graphs/dfs.py
@@ -0,0 +1,12 @@
+def dfs(graph, start_node):
+    stack = [start_node]
+    visited = []
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+
+            for i in range(len(graph[node])-1,-1,-1):
+                if graph[node][i] == 1:
+                    stack.append(i)
+    return visited
