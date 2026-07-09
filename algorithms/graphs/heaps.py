3d9a58cf731dbd41c912da8bca6ad55788ed91a8
ashwija-s <ashwijashetty06@gmail.com>
2026-07-09 13:28:16 +0530

    Add heap example

---
+++ algorithms/graphs/heaps.py
@@ -0,0 +1,25 @@
+import heapq
+
+my_heap = []
+heapq.heappush(my_heap, 9)
+heapq.heappush(my_heap, 8)
+heapq.heappush(my_heap, 7)
+print(my_heap)  #[7, 9, 8]
+
+heapq.heappop(my_heap)
+print(my_heap)  #[8, 9]
+
+heapq.heappushpop(my_heap, 7)
+print(my_heap)   #[]
+
+nums = [5, 7, 3, 1]
+heapq.heapify(nums)
+print(nums)  #[1, 5, 3, 7]
+
+my_heap = []
+heapq.heappush(my_heap, (3, "A"))
+heapq.heappush(my_heap, (2, "B"))
+heapq.heappush(my_heap, (1, "A"))
+print(my_heap)  #[(1, 'A'), (3, 'A'), (2, 'B')]
+
+print(heapq.heappop(my_heap))  #(1, 'A')
