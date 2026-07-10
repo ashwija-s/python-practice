d33708c88f82a2d4edf70327980007fd945403b9
ashwija-s <ashwijashetty06@gmail.com>
2026-07-10 12:24:36 +0530

    Add fibonacci calculator code.

---
+++ algorithms/dynamic_programming/fibonacci_calculator.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    sequence = [0,1]
+    if n < 2:
+        return n
+    for i in range(2, n + 1):
+        sequence.append(sequence[i-1] + sequence[i-2])
+    return sequence[n]
+print(fibonacci(13))  #233
