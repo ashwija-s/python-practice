62bb83388b2d33a2fbc5c628fd13537e3a4cbcea
ashwija-s <ashwijashetty06@gmail.com>
2026-07-18 09:55:43 +0530

    Refractor CLI code

--- mini_projects/contact_book_cli.py
+++ mini_projects/contact_book_cli.py
@@ -1,38 +1,54 @@
 contacts = {}
-while True:
+def menu():
     print("===== CONTACT BOOK =====\n1. Add Contact \n2. View Contacts \n3. Search Contact \n4. Delete Contact \n5. Exit")
+
+def add_contact():
+    name = input("Enter name: ")
+    contact = input("Enter contact: ")
+    contacts[name] = contact
+    print("Contact added successfully.")
+
+def view_contact():
+    if not contacts:
+        print("No contacts found.")
+    else:
+        for name, contact in contacts.items():
+            print(f"{name}: {contact}")
+            print(" 1")
+def search_contact():
+    name = input("Enter name: ")
+    if name in contacts:
+        print(contacts[name])
+    else:
+        print("Contact not found.")
+def delete_contact():
+    name = input("Enter name: ")
+    if name in contacts:
+        del contacts[name]
+        print("Contact deleted.")
+    else:
+        print("Contact not found.")
+while True:
+    menu()
     while True:
         try:    
             x = int(input("Enter the number: "))
             break
         except ValueError:
             print("Please enter a number.")
+
+
     if x == 1:
-        name = input("Enter name: ")
-        contact = input("Enter contact: ")
-        contacts[name] = contact
-        print("Contact added successfully.")
+        add_contact()
 
     elif x == 2:
-        if not contacts:
-            print("No contacts found.")
-        for i, k in contacts.items():
-                print(f"{i}: {k}")
+        view_contact()
 
     elif x == 3:
-        name = input("Enter name: ")
-        if name in contacts:
-            print(contacts[name])
-        else:
-            print("Contact not found.")
+        search_contact()
 
     elif x == 4:
-        name = input("Enter name: ")
-        if name in contacts:
-            del contacts[name]
-            print("Contact deleted.")
-        else:
-            print("Contact not found.")
+        delete_contact()
 
     elif x == 5:
         break
